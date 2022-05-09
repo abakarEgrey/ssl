64b4b1b0f24694f0f06fea7f22297c28cc3b9afe
Abakar MAHAMAT SOUGUI <egreabakar@yahoo.fr>
2022-05-10 00:28:36 +0200

    Updated restapi.sh file allows to call the rest api with an url as parameter

--- restapi.sh
+++ restapi.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -x
 
+if [ "$#" -ne 1 ]; then
+  echo "you must give one argument : for example https://swisscom.com"
+  exit 1
+fi
+
 curl --location --request POST 'http://localhost:8080/ssl/certificate/fetch' \
 --header 'Content-Type: application/json' \
 --data-raw '{
